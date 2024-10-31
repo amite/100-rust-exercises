b3d6896ff0e5ec2bae117deba4cf96ad02c82cc1
amite <amit.erandole@gmail.com>
2024-10-31 15:27:06 +0530

    beginning traits

--- exercises/03_ticket_v1/12_outro/src/lib.rs
+++ exercises/03_ticket_v1/12_outro/src/lib.rs
@@ -11,3 +11,75 @@
 // Integration here has a very specific meaning: they test **the public API** of your project.
 // You'll need to pay attention to the visibility of your types and methods; integration
 // tests can't access private or `pub(crate)` items.
+//
+
+pub struct Order {
+    product_name: String,
+    quantity: u16,
+    unit_price: u16,
+}
+
+impl Order {
+    pub fn new(product_name: String, quantity: u16, unit_price: u16) -> Order {
+        validate_product_name(&product_name);
+        validate_quantity(&quantity);
+        validate_unit_price(&unit_price);
+        Order {
+            product_name,
+            quantity,
+            unit_price,
+        }
+    }
+
+    pub fn product_name(&self) -> &String {
+        &self.product_name
+    }
+
+    pub fn quantity(&self) -> &u16 {
+        &self.quantity
+    }
+
+    pub fn unit_price(&self) -> &u16 {
+        &self.unit_price
+    }
+
+    pub fn total(&self) -> u16 {
+        self.quantity() * self.unit_price()
+    }
+
+    pub fn set_product_name(&mut self, new_product_name: String) {
+        validate_product_name(&new_product_name);
+        self.product_name = new_product_name
+    }
+
+    pub fn set_quantity(&mut self, new_quantity: u16) {
+        validate_quantity(&new_quantity);
+        self.quantity = new_quantity
+    }
+    pub fn set_unit_price(&mut self, new_unit_price: u16) {
+        validate_unit_price(&new_unit_price);
+        self.unit_price = new_unit_price;
+    }
+}
+
+fn validate_product_name(product_name: &String) {
+    if product_name.is_empty() {
+        panic!("Product name can't be empty");
+    }
+
+    if product_name.len() > 300 {
+        panic!("Product name exceeds 300 bytes");
+    }
+}
+
+fn validate_quantity(quantity: &u16) {
+    if *quantity == 0u16 {
+        panic!("Quantity can't be zero");
+    }
+}
+
+fn validate_unit_price(unit_price: &u16) {
+    if *unit_price == 0u16 {
+        panic!("Price can't be zero");
+    }
+}
