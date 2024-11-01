1d032d7f0aa425abcb529e28f6f5fc0b4e460d71
amite <amit.erandole@gmail.com>
2024-11-01 10:36:27 +0530

    implementing drop

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,30 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+pub struct DropBomb {
+    defused: bool,
+}
+
+impl DropBomb {
+    pub fn new() -> DropBomb {
+        DropBomb { defused: false }
+    }
+
+    pub fn defuse(&mut self) {
+        self.defused = true;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        // We don't need to do anything here,
+        // it's enough to have an "empty" Drop implementation
+        if self.defused == false {
+            panic!("I have not been defused");
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
